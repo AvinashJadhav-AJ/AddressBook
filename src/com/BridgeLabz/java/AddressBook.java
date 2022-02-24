db6f88456edf38c654336b17bc3e857ee68f0b79
Avinash <aj912873@gmail.com>
2022-02-24 14:20:06 +0530

    Added: Added New Contact To Address Book

--- src/com/BridgeLabz/java/AddressBook.java
+++ src/com/BridgeLabz/java/AddressBook.java
@@ -1,8 +1,50 @@
 package com.BridgeLabz.java;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
 	public static void main(String[]Args) {
-		System.out.println("Welcome To AddressBook");
+		ArrayList<Contact> array = new ArrayList<Contact>();
+		try (Scanner sc = new Scanner(System.in)) {
+			try (Scanner sc1 = new Scanner(System.in)) {
+				int choice;
+				do {
+					System.out.println("1.Adding New Contact");
+					System.out.println("2.Check Added Contact");
+					System.out.println("Enter your choice");
+					choice = sc.nextInt();
+
+					switch (choice) {
+					case 1:
+						System.out.print("Enter First Name: ");
+						String firstName = sc1.nextLine();
+						System.out.print("Enter last Name: ");
+						String lastName = sc1.nextLine();
+						System.out.print("Enter your address: ");
+						String address = sc1.nextLine();
+						System.out.print("Enter your city: ");
+						String city = sc1.nextLine();
+						System.out.print("Enter your state: ");
+						String state = sc1.nextLine();
+						System.out.print("Enter zip code : ");
+						int zipcode = sc.nextInt();
+						System.out.print("Enter phone number: ");
+						String phoneNo = sc1.nextLine();
+						System.out.print("Enter your email: ");
+						String email = sc1.nextLine();
+						Contact contact = new Contact(firstName, lastName, address, city, zipcode, state, phoneNo,
+								email);
+						array.add(contact);
+						break;
+					case 2:
+						System.out.println(array);
+						break;
+					}
+				} while (choice != 0);
+				System.out.println("Invalid Choice!!!");
+			}
+		}
 	}
 
 }
